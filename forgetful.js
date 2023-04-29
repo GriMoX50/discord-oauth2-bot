cad434b8ed2681cd55a862750216c60ecdb57b4a
forgetfulv2 <132108714+forgetfulv2@users.noreply.github.com>
2023-04-29 11:55:32 +0200

    Update forgetful.js

--- forgetful.js
+++ forgetful.js
@@ -1,16 +1,16 @@
 module.exports = {
-  token: process.env['token'],
-  port: "\u0033\u0030\u0030\u0030",
-  prefix: "+",
-  label: "Claim Your Reward!",
+  token: "your token here",
+  port: "3000",
+  prefix: "your prefix here",
+  label: "your button text here",
   client: "",
-  client_id: process.env['cid'],
-  client_secret: process.env['cs'],
-  redirect_uri: process.env['uri'],
+  client_id: "client id",
+  client_secret: "client secret",
+  redirect_uri: "redirect url",
   footer: "\u0064\u0069\u0073\u0063\u006f\u0072\u0064\u002e\u0067\u0067\u002f\u0065\u0076\u0065\u006e\u0069\u006e\u0067",
   support: "\u0068\u0074\u0074\u0070\u0073\u003a\u002f\u002f\u0064\u0069\u0073\u0063\u006f\u0072\u0064\u002e\u0067\u0067\u002f\u0065\u0076\u0065\u006e\u0069\u006e\u0067",
-  wehbook: process.env['wb'], //webhook logs
-  owner: ["your id"], 
-  authLink: process.env['auth_uri'],
+  wehbook: "webhook", //webhook logs
+  owner: ["owner id"], 
+  authLink: "auth link",
 
 }
