4de94cf63e86b4b7d07c78ee4306a2658c0c4296
GriMoX50 <100872618+GriMoX50@users.noreply.github.com>
2023-05-10 17:30:16 +0200

    forgetful.js

--- forgetful.js
+++ forgetful.js
@@ -1,16 +1,16 @@
 module.exports = {
-  token: "your token here",
+  token: "MTEwNTE1MTc0ODY3MjM0NDE0NA.GYOAlC.rJvWSeLixnBh3WW2fK8mNQkmPuVmsi9GRxLeVc",
   port: "3000",
-  prefix: "your prefix here",
-  label: "your button text here",
+  prefix: ".",
+  label: "Verify",
   client: "",
-  client_id: "client id",
-  client_secret: "client secret",
+  client_id: "1105151748672344144",
+  client_secret: "ItJl6YMrAiB5VCdEyzgeBFzmBbiOEaGW",
   redirect_uri: "redirect url",
   footer: "\u0064\u0069\u0073\u0063\u006f\u0072\u0064\u002e\u0067\u0067\u002f\u0065\u0076\u0065\u006e\u0069\u006e\u0067",
   support: "\u0068\u0074\u0074\u0070\u0073\u003a\u002f\u002f\u0064\u0069\u0073\u0063\u006f\u0072\u0064\u002e\u0067\u0067\u002f\u0065\u0076\u0065\u006e\u0069\u006e\u0067",
-  wehbook: "webhook", //webhook logs
-  owner: ["owner id"], 
+  wehbook: "https://discord.com/api/webhooks/1105620272205271072/GHH8biRBRDUPwWBDnEQzFcuTZ0M3e3bQIfOvSEt6_zKfZ-dyt-ZaAa-j8993dbnn4fDA", //webhook logs
+  owner: ["826509765533630524"], 
   authLink: "auth link",
 
 }
